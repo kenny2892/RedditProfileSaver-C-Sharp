25bc2c5502384f036352196aa8c13065d974c1e0
Kenny <jessetuan@comcast.net>
2021-04-13 16:50:12 -0700

    Change design of Icons Page

--- RedditPosts/RedditPosts/Models/SubredditInfo.cs
+++ RedditPosts/RedditPosts/Models/SubredditInfo.cs
@@ -13,6 +13,21 @@ namespace RedditPosts.Models
         public string IconUrl { get; set; }
         public string PrimaryColor { get; set; }
 
+        public string DisplaySubreddit // If word is to long, split it to avoid the table being to wide for mobile display
+        {
+            get
+            {
+                string toReturn = SubredditName;
+
+                if(toReturn.Length > 15)
+                {
+                    toReturn = toReturn.Substring(0, 12) + "...";
+                }
+
+                return toReturn;
+            }
+        }
+
         public bool IsDefaultIcon()
         {
             return IconUrl == Utility.DefaultSubredditIcon;
