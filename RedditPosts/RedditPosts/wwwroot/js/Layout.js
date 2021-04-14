b507ae99e95f25c70ebe9b1c103f6ed1682b6021
Kenny <jessetuan@comcast.net>
2021-04-13 21:18:46 -0700

    Developing Navigation Pin

--- RedditPosts/RedditPosts/wwwroot/js/Layout.js
+++ RedditPosts/RedditPosts/wwwroot/js/Layout.js
@@ -1,6 +1,4 @@
-﻿// Source: https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
-
-function SetupThemeToggle()
+﻿function SetupThemeToggle() // Source: https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
 {
     const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
     const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
@@ -32,4 +30,28 @@ function SetupThemeToggle()
     }
 
     toggleSwitch.addEventListener('change', switchTheme, false);
+}
+
+function SetupPinToggle()
+{
+    const navbarPinBtn = document.querySelector('.pin-nav-btn');
+    const navbar = document.querySelector('.navbar');
+    const body = document.querySelector('.content-body');
+
+    function toggle(e)
+    {
+        if(e.target.checked)
+        {
+            navbar.classList.add("fixed-top");
+            body.setAttribute("style", "padding-top: 80px;");
+        }
+
+        else
+        {
+            navbar.classList.remove("fixed-top");
+            body.setAttribute("style", "padding-top: none;");
+        }
+    }
+
+    navbarPinBtn.addEventListener('change', toggle, false);
 }
